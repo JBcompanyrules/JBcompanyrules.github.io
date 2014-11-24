4fe2abc4f9fee3615fb18a0416527b1f108bb919
Jacob Bondley <hasharahha@gmail.com>
2014-11-24 18:58:10 -0500

    Create main.js

---
+++ login/main.js
@@ -0,0 +1,21 @@
+var user;
+
+function Login(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+
+if(username=="hasharahha@gmail.com"&&password=="JAB"){
+Sign(jacob);
+}
+else{
+alert("Incorrect username or password");
+}
+
+
+}
+function Sign(user){
+if(user==="jacob"){
+alert("welcome Jacob");
+}
+
+}
