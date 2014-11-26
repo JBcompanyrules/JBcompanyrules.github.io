22090e981dee36fcf221aacbc47fee2e16307e93
Jacob Bondley <hasharahha@gmail.com>
2014-11-26 10:24:06 -0500

    Update symbols.js

--- pages/symbols.js
+++ pages/symbols.js
@@ -1,6 +1,16 @@
 var symbols=["♠", "Ω", "♤", "♣",  "♧", "♥", "♡", "♦", "♢", "♔", "♕", "♚", "♛", "★", "☆", "✮",  "✯", "☀", "☁", "☺", "۞", "۩","➀", "➁", "➂", "➃", "➄", "➅", "➆", "➇", "➈", "➉", "➊", "➋", "➌", "➍", "➎", "➏", "➐", "➑", "➒", "➓","εїз", "Ƹ̵̡Ӝ̵̨̄", "ξЖЗ", "εжз", "☎", "☏", "¢", "☚", "☛", "☜", "☝", "☞", "☟", "✍", "✌", "♨", "๑", "❀", "✿", "ψ", "♪", "♩", "♬", "✄", "✂", "✆", "✉", "✦", "✧"];
 var rnd;
+var id;
 function Find(){
   rnd = Math.floor(Math.random()*60)
   document.getElementById("symbol").innerHTML=symbols[rnd];
+}
+
+function log(msg){
+  confirm("Console: \n"+msg)
+}
+
+function FindP(){
+  id = document.getElementById("Id").value
+  document.getElementById("symbol").innerHTML=id;
 }
