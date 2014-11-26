069b9f84c339a4923bbbba0e98878040d620f35d
Jacob Bondley <hasharahha@gmail.com>
2014-11-26 00:21:04 -0500

    Update symbols.js

--- pages/symbols.js
+++ pages/symbols.js
@@ -1,13 +1,7 @@
 var symbols=["♠", "Ω", "♤", "♣",  "♧", "♥", "♡", "♦", "♢", "♔", "♕", "♚", "♛", "★", "☆", "✮",  "✯", "☀", "☁", "☺", "۞", "۩"];
 var Combo =["➀", "➁", "➂", "➃", "➄", "➅", "➆", "➇", "➈", "➉", "➊", "➋", "➌", "➍", "➎", "➏", "➐", "➑", "➒", "➓"];
-var rnd =2;
-alert("Hello"+symbols[rnd]);
-function update(event){
-if(13 === event.keyCode){
-Check();
-}
-}
-
-function Check(){
-
+var rnd;
+function Find(){
+  rnd = Math.floor(Math.random()*16)
+  document.getElementById("symbol").innerHTML=symbols[rnd];
 }
