81f5a1467ccec7a0028f69ce599b59abcbfa139c
Jacob Bondley <hasharahha@gmail.com>
2014-11-27 11:51:12 -0500

    Update main_save.js

--- main_save.js
+++ main_save.js
@@ -1,12 +1,15 @@
+alert("welcome");
 var data;
 var userdata;
 var usercache;
+setTimeout("Check()", "1000");
 setInterval("Save()", "200");
+function Check(){
 if(localStorage.getItem("saved")=="true"){
 userdata = localStorage.getItem("data");
 document.getElementById("userdata").value=userdata;
 }
-
+}
 function Save(){
 data = document.getElementById("userdata").value;
 localStorage.setItem("data", data);
