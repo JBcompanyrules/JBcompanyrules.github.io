5ba71d00c0fe70f69c17a255e1038d2e2f04f4e4
Jacob Bondley <hasharahha@gmail.com>
2014-11-27 23:39:33 -0500

    Update date.js

--- process/date.js
+++ process/date.js
@@ -1,6 +1,10 @@
 var Date = new Date();
-
+var dates =["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var weeks =["Sunday", "Monday", "Tuesday", "Wendsday", "Thursday", "Friday", "Saturday"];
 setText("calender-month", dates[Date.getMonth()]);
+setText("calender-week", weeks[Date.getWeek()]);
+setText("calender-day", Date.getWeek());
+setText("calender-year", Date.getDate()+1900);
 
 function setText(id, val){
 if(val < 10){
