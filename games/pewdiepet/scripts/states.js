1d44428623df03990f49c82a7c32eb9811da94eb
Jacob Bondley <hasharahha@gmail.com>
2014-12-02 19:04:55 -0500

    Update states.js

--- games/pewdiepet/scripts/states.js
+++ games/pewdiepet/scripts/states.js
@@ -1,1 +1,10 @@
 //Player States
+
+var player ={
+  coins:"0",
+  xp:"0",
+  health:"50",
+  start: funtion(){ var username = prompt("What is your pets name", "PewDiePet023") },
+  Cache:["0", "0", "0"],
+  state:"baby"
+}
