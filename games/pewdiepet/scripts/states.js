74dcea8c88fbf04829db3560e4747612fd3f1350
Jacob Bondley <hasharahha@gmail.com>
2014-12-02 19:33:29 -0500

    Update states.js

--- games/pewdiepet/scripts/states.js
+++ games/pewdiepet/scripts/states.js
@@ -1,18 +1,18 @@
 //Player States
-var PlayerState = document.getElementById("playerstatus");
+
 function Check(){
   if(player.state ==="baby"){
-    PlayerState.cx="40";
-    PlayerState.cy="40";
-    PlayerState.r="30";
+    document.getElementById("playerstatus").cx="40";
+    document.getElementById("playerstatus").cy="40";
+    document.getElementById("playerstatus").r="30";
   }
 }
 var player ={
   coins:"0",
   xp:"0",
   health:"50",
-  start: function(){ var username = prompt("What is your pets name", "PewDiePet023"); },
-  Cache:["0", "0", "0"],
+  start: function(){var username =prompt("What is your pets name", "PewDiePet023");},
+  Cache:[],
   state:"baby"
   check: Check()
 }
