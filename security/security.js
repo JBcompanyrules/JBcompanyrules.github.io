8344be0e0e5802a8ac1cc45a298d83f8a3e7c23c
Jacob Bondley <hasharahha@gmail.com>
2014-12-19 14:26:00 -0500

    Create security.js

---
+++ security/security.js
@@ -0,0 +1,13 @@
+//HAHA
+var static;
+function Check(){
+static = prompt("Check network, Give command");
+if(static =! null){
+call();
+}
+else{
+alert("Network, unknown command");
+}
+function call(){
+
+}
