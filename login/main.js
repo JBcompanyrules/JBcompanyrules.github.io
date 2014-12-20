55fedb56eb2b71aa75b5d1bfaedf1d973396112b
Jacob Bondley <hasharahha@gmail.com>
2014-12-20 13:12:21 -0500

    Update main.js

--- login/main.js
+++ login/main.js
@@ -4,20 +4,8 @@ function Login(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 
-if(username=="hasharahha@gmail.com"&&password=="JAB"){
-user ="Jacob,Bondley";
-document.getElementById("user").innerHTML=user
-document.getElementById("Login").style.display='none'
-alert("Jacob has signed in");
-}
-else{
-alert("Incorrect username or password");
-}
-}
-
-function Check(event){
-  if(13 === event.keyCode){
-    Login();
-  }
+if(usernames[username]&&password ==usernames[username][0){
+  user = username[username];
+  alert(user+" Has logged in");
 }
 
