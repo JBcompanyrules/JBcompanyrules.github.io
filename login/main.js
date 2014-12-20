0b3eb2d0940589b444db6df7b81494516e17bc2e
Jacob Bondley <hasharahha@gmail.com>
2014-12-20 13:14:41 -0500

    Update main.js

--- login/main.js
+++ login/main.js
@@ -1,10 +1,10 @@
 var user;
 
-function Login(){
+function login(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 
-if(usernames[username]&&password ==usernames[username][0){
+if(usernames[username]&&password ==usernames[username][0]){
   user = username[username];
   alert(user+" Has logged in");
 }
