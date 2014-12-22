a9fd0eb3ce42279f71903b2393c4f4ac3140c890
Jacob Bondley <hasharahha@gmail.com>
2014-12-22 10:15:04 -0500

    Update SPC.js

--- mods/SPC.js
+++ mods/SPC.js
@@ -1,2 +1,15 @@
 //Made by HASHARAHHA14
 var static;
+var home =[];
+var homeset =false;
+var commands ={
+  "help":[1, 2],
+  "give":["itemId", "Amount", "Damage"],
+  "gamemode": [0, 1],
+  "settime": ["Time#"],
+  "explode": ["radius"],
+  "sethome": [playerX, playerY, playerZ, true],
+  "delhome": [0, 0, 0, false],
+  "home": ["home[0]", "home[1]", "home[2]"],
+  "kill": ["player.setHealth(0)"]
+};
